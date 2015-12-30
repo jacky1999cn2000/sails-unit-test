c972b9bcd14780001e83a52ea0a5ca982ab72e70
Jacky Zhao <jacky.zhao@bunchball.com>
2015-12-29 20:39:08 -0800

    config files

---
+++ config/env/unitTest.js
@@ -0,0 +1,30 @@
+/**
+ * Unit Testing environment settings
+ *
+ * This file can include shared settings for a production environment,
+ * such as API keys or remote database passwords.  If you're using
+ * a version control solution for your Sails app, this file will
+ * be committed to your repository unless you add it to your .gitignore
+ * file.  If your repository will be publicly viewable, don't add
+ * any private information to this file!
+ *
+ */
+
+module.exports = {
+
+  connections: {
+    unitTest: {
+      adapter: 'sails-disk'
+    }
+  },
+
+  port: 3002,
+
+  log: {
+    level: 'error'
+  },
+
+  models: {
+    connection: 'unitTest'
+  }
+};
